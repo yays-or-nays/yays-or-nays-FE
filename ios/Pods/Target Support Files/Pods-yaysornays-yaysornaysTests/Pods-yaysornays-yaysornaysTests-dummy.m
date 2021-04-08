978da348b89609a06a93a880bdbeeb16dabac3e1
Luke Mason <lukjmason@gmail.com>
2021-04-08 17:05:11 -0600

    Delete ios/pods from .gitignore

---
+++ ios/Pods/Target Support Files/Pods-yaysornays-yaysornaysTests/Pods-yaysornays-yaysornaysTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_yaysornays_yaysornaysTests : NSObject
+@end
+@implementation PodsDummy_Pods_yaysornays_yaysornaysTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-yaysornays/Pods-yaysornays-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_yaysornays : NSObject
+@end
+@implementation PodsDummy_Pods_yaysornays
+@end
