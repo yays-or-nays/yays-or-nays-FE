0a0bf8ccc95fa9e01335f5b90b89bc0eb6fb8f5b
mattdean <matthewallandean@gmail.com>
2021-04-11 16:04:30 -0600

    Mock up categories page

--- src/Categories/Categories.js
+++ src/Categories/Categories.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import {StyleSheet, Header, ScrollView, Text, Button, View, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  Text,
+  Button,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 
-const Categories = () => (
-  <View>
-    <Text style={styles.header}>Pick a Category</Text>
-    <ScrollView>
-      <TouchableOpacity style={styles.category}>
-        <Text style={styles.categoryText}>Entertainment</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.category}>
-        <Text style={styles.categoryText}>Animals</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.category}>
-        <Text style={styles.categoryText}>Food</Text>
-      </TouchableOpacity>
-    </ScrollView>
-  </View>
-);
+const Categories = () => {
+  // touch a category, open the Swiper component with the corresponding category info
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handlePress = id => {
+    setSelectedCategory(id);
+    console.log(selectedCategory);
+  };
+
+  return (
+    <View>
+      <Text style={styles.header}>Pick a Category</Text>
+      <ScrollView>
+        <TouchableOpacity
+          id="entertainment"
+          style={styles.category}
+          onPress={() => handlePress('entertainment')}>
+          <Text style={styles.categoryText}>Entertainment</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          id="food"
+          style={styles.category}
+          onPress={() => handlePress('food')}>
+          <Text style={styles.categoryText}>Food</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          id="animals"
+          style={styles.category}
+          onPress={() => handlePress('animals')}>
+          <Text style={styles.categoryText}>Animals</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+};
 
 export default Categories;
 
