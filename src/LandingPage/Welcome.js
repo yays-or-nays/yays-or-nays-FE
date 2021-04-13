6cdadd1c8a67c146c2e25700f3b601f53db050ce
Luke Mason <lukjmason@gmail.com>
2021-04-13 11:31:41 -0600

    Create Welcome component

---
+++ src/LandingPage/Welcome.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import {Redirect, NativeRouter, Route, Link} from 'react-router-native';
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View,
+} from 'react-native';
+
+const WelcomePage = () => {
+  
+}
