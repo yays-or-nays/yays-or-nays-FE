4cb23ad1d3f59d1a53acab494ee75d97bae0877b
Luke Mason <lukjmason@gmail.com>
2021-04-13 11:51:49 -0600

    Get app logo on landing page

--- src/LandingPage/Welcome.js
+++ src/LandingPage/Welcome.js
@@ -5,11 +5,14 @@ import {
   Text,
   Button,
   View,
+  Image
 } from 'react-native';
+import logo from '../../ios/yaysornays/Images.xcassets/Image.imageset/appLogo.png';
 
 const WelcomePage = () => {
   return (
     <View>
+      <Image source={logo} />
       <Text>Welcome</Text>
     </View>
   )
