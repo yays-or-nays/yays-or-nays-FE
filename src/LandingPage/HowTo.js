b6fb4af7dbb8c9d7ff42073fd7c08bdb9ace6511
Luke Mason <lukjmason@gmail.com>
2021-04-15 10:04:12 -0600

    Complete slide 2 of HowTo page

--- src/LandingPage/HowTo.js
+++ src/LandingPage/HowTo.js
@@ -18,12 +18,12 @@ const HowTo = () => {
           <Text style={styles.text}>An app that allows you to answer the world's most important questions like...</Text>
           <Text style={styles.hotTakeText}>"Is Soup a Drink?"</Text>
           <Text style={styles.text}>Then compare your answers to the world of people to see if you're with the people...or against them.</Text>
+          <Text style={{marginTop: '35%'}}>Swipe right to continue >></Text>
         </View >
         <View style={styles.view}>
-          <Text style={styles.headerText}>Welcome to HotTakes!</Text>
-          <Text style={styles.text}>An app that allows you to answer the world's most important questions like...</Text>
-          <Text style={styles.hotTakeText}>"Is Soup a Drink?"</Text>
-          <Text style={styles.text}>Then compare your answers to the world of people to see if you're with the people...or against them.</Text>
+          <Text style={styles.text}>Once you click 'Get Started', you will be taken to the main dashboard where you will see a list of categories.</Text>
+          <Text style={styles.text}>**INSERT SCREENSHOT HERE**</Text>
+          <Text style={styles.text}>You can choose either of the categories to answer hot takes related to that specific topic.</Text>
         </View>
       </Swiper>
   )
