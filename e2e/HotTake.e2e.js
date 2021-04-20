8cd609d1f5d5d9ff4f998f1d9dc24a0a01e53b91
Brian Forbes <brianandrewf81@gmail.com>
2021-04-20 17:30:19 -0600

    Test user can select a category of hot takes and swipe to vote on it

--- e2e/HotTake.e2e.js
+++ e2e/HotTake.e2e.js
@@ -4,13 +4,22 @@ describe('App', () => {
     await device.reloadReactNative();
   });
 
-  it('should be able to select a category and vote on hot takes', async () => {
+  it('should be able to view all categories of hot takes', async () => {
     await element(by.text('Get Started')).tap();
     await expect(element(by.text('Entertainment'))).toBeVisible();
     await expect(element(by.text('Food & Beverage'))).toBeVisible();
     await expect(element(by.text('Software Development'))).toBeVisible();
     await expect(element(by.text('Miscellaneous'))).toBeVisible();
+  });
+
+  it('should be able to select a category and vote on hot takes', async () => {
     await element(by.text('Entertainment')).tap();
+    await expect(element(by.id('text'))).toBeVisible();
+    await element(by.id('card')).swipe('right')
+  });
+
+  it('should be able to see votes for and against a hot take after voting', async () => {
+    await expect(element(by.text('Yes Votes'))).toBeVisible();
   });
 
 });
